856359de1de1d3bfc04db850247cf2c31ee41136
Ayushman <maanayush1002@gmail.com>
2023-01-20 23:31:21 +0530

    Constructor overloading

---
+++ constructorOverloading.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Complex
+{
+    int a;
+    int b;
+    public:
+    Complex(void)
+    {
+        a=0;
+        b=0;
+    }
+    Complex(int x, int y)
+    {
+        a=x;
+        b=y;
+    }
+    Complex(int x)
+    {
+        a=x;
+        b=0;
+    }
+    void PrintNumber()
+    {
+        cout<<a<<"  + i"<<b<<endl;
+    }
+};
+
+int main(){
+
+    Complex c1(10, 2);
+    c1.PrintNumber();
+
+    Complex c2(10);
+    c2.PrintNumber();
+
+    Complex c3;
+    c3.PrintNumber();
+
+return 0;
+}
